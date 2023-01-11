e5d921f742d1f4e835859afdd2bcf0503b45dfe0
gary909 <garywhite61@hotmail.com>
2023-01-11 14:08:22 +0100

    code

---
+++ app.js
@@ -0,0 +1,11 @@
+// var ArrowFunc = function (arr) {
+//     // return arr.map( => ).join(''); //Complete this function
+//     function 
+// }
+
+function ArrowFunction(arr) {
+    return arr;
+}
+
+console.log(ArrowFunc([84, 101, 115, 116])); // 'Test'
+console.log(ArrowFunc([70, 85, 83, 32, 82, 79, 72, 32, 68, 65, 72])); // 'Test'
